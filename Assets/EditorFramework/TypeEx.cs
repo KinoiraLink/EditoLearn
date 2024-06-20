fd6e1ec4fde01da435839279d4b7001d4715fa20
KinoiraLink <summerkino@outlook.com>
2024-06-20 22:07:01 +0800

    反射搜索优化

--- Assets/EditorFramework/TypeEx.cs
+++ Assets/EditorFramework/TypeEx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Unity.VisualScripting;
 
 namespace EditorFramework
 {
@@ -10,6 +11,7 @@ namespace EditorFramework
         public static IEnumerable<Type> GetSubTypesInAssemblies(this Type self)
         {
             return AppDomain.CurrentDomain.GetAssemblies()
+                .Where(assembly => assembly.FullName.StartsWith("Assembly"))
                 .SelectMany(assembly => assembly.GetTypes())
                 .Where(type => type.IsSubclassOf(self));
         }
