2addbc79b5af6ae56ee0a5528e2abc8c7407b4b4
KinoiraLink <summerkino@outlook.com>
2024-06-20 11:41:35 +0800

    简易框架搭建

---
+++ Assets/EditorFramework/Editor/GUIBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace EditorFramework
+{
+    public abstract class GUIBase : IDisposable
+    {
+
+        public bool mDisposed { get; private set; }
+        public Rect mPostition { get; private set; }
+
+        public virtual void OnGUI(Rect position)
+        {
+            mPostition = position;
+        }
+
+        public virtual void Dispose()
+        {
+            if (mDisposed) return;
+            OnDispose();
+            mDisposed = true;
+        }
+
+        protected abstract void OnDispose();
+    }
+}
---
+++ Assets/EditorFramework/Example/2.GUIBase/Editor/GUIBaseExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorFramework
+{
+    [CustomEditorWindow]
+    public class GUIBaseExample : EditorWindow
+    {
+        public class Label : GUIBase
+        {
+            private  string mText;
+
+            public Label(string text)
+            {
+                mText = text;
+            }
+
+            public override void OnGUI(Rect position)
+            {
+                GUILayout.Label(mText);
+            }
+            protected override void OnDispose()
+            {
+                mText = null;
+            }
+        }
+
+        private Label mLabel = new Label("123");
+        private Label mLabel2 = new("456");
+
+
+        private void OnGUI()
+        {
+            mLabel.OnGUI(default);
+            mLabel2.OnGUI(default);
+        }
+    }
+}
