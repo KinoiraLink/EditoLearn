436de3687746f0bb85b8d9d8e1e206cdff13d268
KinoiraLink <summerkino@outlook.com>
2024-06-23 21:59:03 +0800

    文件槽的槽绘制 与 路径显示方式统一（修复上一节的Bug）

--- Assets/EditorFramework/Example/5.FolderField/Editor/FolderFieldExample.cs
+++ Assets/EditorFramework/Example/5.FolderField/Editor/FolderFieldExample.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,23 +9,30 @@ namespace EditorFramework
     [CustomEditorWindow(5)]
     public class FolderFieldExample : EditorWindow
     {
-        private string mPath = "String_Empty";
+        private string mPath = "Assets";
         private void OnGUI()
         {
             var rect = EditorGUILayout.GetControlRect(GUILayout.Height(20));
             var rects = rect.VerticalSplit(rect.width - 30);
             var leftRect = rects[0];
             var rightRect = rects[1];
+            
 
-            GUI.Label(leftRect,mPath);
+         
             if (GUI.Button(rightRect, GUIContents.Folder))
             {
                 var path = EditorUtility.OpenFolderPanel("打开文件", Application.dataPath, "default Name");
 
-                mPath = path;
-                Debug.Log(path);
+                var assetsFullPath = Path.GetFullPath(Application.dataPath);
+                mPath = "Assets" + Path.GetFullPath(path).
+                    Substring(assetsFullPath.Length)
+                    .Replace("\\","/");
+                Debug.Log(mPath);
             }
-
+            var currentGUIEnable = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.TextField(leftRect,mPath);
+            GUI.enabled = currentGUIEnable;
             var dragInfo = DragAndDropTool.Drag(Event.current, leftRect);
 
             if (dragInfo.enterArea && dragInfo.complete && !dragInfo.dragging)
