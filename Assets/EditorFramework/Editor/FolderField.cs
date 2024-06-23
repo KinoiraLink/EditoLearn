e91ac8e497f5586b93aa6a9f3cfe995cadd704ff
KinoiraLink <summerkino@outlook.com>
2024-06-23 22:55:36 +0800

    FoldeField 组件封装

---
+++ Assets/EditorFramework/Editor/FolderField.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EditorFramework
+{
+    public class FolderField : GUIBase
+    {
+        protected string mPath ;
+        public string Path => mPath;
+        public void SetPath(string path) => mPath = path;
+        public string Title;
+        public string Folder;
+        public string DefaultName;
+        public FolderField(string path = "Assets", string folder = "Assets", string title = "Select Folder",
+            string defaultName = "")
+        {
+            mPath = path;
+            Title = title;
+            Folder = folder;
+            DefaultName = defaultName;
+
+        }
+
+        public override void OnGUI(Rect position)
+        {
+            base.OnGUI(position);
+            
+            var rects = position.VerticalSplit(position.width - 30);
+            var leftRect = rects[0];
+            var rightRect = rects[1];
+            
+
+         
+            if (GUI.Button(rightRect, GUIContents.Folder))
+            {
+                var path = EditorUtility.OpenFolderPanel(Title, Folder, DefaultName);
+
+                if (!string.IsNullOrEmpty(path) && path.IsDirectory())
+                {
+                    mPath = path.ToAssetsPath();
+                
+                    Debug.Log(mPath);    
+                }
+                
+            }
+            var currentGUIEnable = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.TextField(leftRect,mPath);
+            GUI.enabled = currentGUIEnable;
+            var dragInfo = DragAndDropTool.Drag(Event.current, leftRect);
+
+            if (dragInfo.enterArea && dragInfo.complete && !dragInfo.dragging && dragInfo.Paths[0].IsDirectory())
+            {
+                mPath = dragInfo.Paths[0];
+            }
+        }
+
+        protected override void OnDispose()
+        {
+        }
+    }
+}
--- Assets/EditorFramework/Example/5.FolderField/Editor/FolderFieldExample.cs
+++ Assets/EditorFramework/Example/5.FolderField/Editor/FolderFieldExample.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -9,36 +10,19 @@ namespace EditorFramework
     [CustomEditorWindow(5)]
     public class FolderFieldExample : EditorWindow
     {
-        private string mPath = "Assets";
+
+        private FolderField mFolderField;
+
+        private void OnEnable()
+        {
+            mFolderField = new FolderField();
+        }
+
         private void OnGUI()
         {
+            GUILayout.Label("Folder Field");
             var rect = EditorGUILayout.GetControlRect(GUILayout.Height(20));
-            var rects = rect.VerticalSplit(rect.width - 30);
-            var leftRect = rects[0];
-            var rightRect = rects[1];
-            
-
-         
-            if (GUI.Button(rightRect, GUIContents.Folder))
-            {
-                var path = EditorUtility.OpenFolderPanel("打开文件", Application.dataPath, "default Name");
-
-                var assetsFullPath = Path.GetFullPath(Application.dataPath);
-                mPath = "Assets" + Path.GetFullPath(path).
-                    Substring(assetsFullPath.Length)
-                    .Replace("\\","/");
-                Debug.Log(mPath);
-            }
-            var currentGUIEnable = GUI.enabled;
-            GUI.enabled = false;
-            EditorGUI.TextField(leftRect,mPath);
-            GUI.enabled = currentGUIEnable;
-            var dragInfo = DragAndDropTool.Drag(Event.current, leftRect);
-
-            if (dragInfo.enterArea && dragInfo.complete && !dragInfo.dragging)
-            {
-                mPath = dragInfo.Paths[0];
-            }
+            mFolderField.OnGUI(rect);
         }
     }
     
