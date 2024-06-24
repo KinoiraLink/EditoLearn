65387290fa8b145d32441eee32e5d321e4c57088
KinoiraLink <summerkino@outlook.com>
2024-06-24 13:16:14 +0800

    XMLGUI 准备 && Label 实现绘制

---
+++ Assets/EditorFramework/Example/7.XMLGUI/Editor/XMLGUIExample.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+using System.Xml;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable CheckNamespace
+
+
+
+// ReSharper disable InconsistentNaming
+
+namespace EditorFramework
+{
+    [CustomEditorWindow(7)]
+    public class XMLGUIExample : EditorWindow
+    {
+        private const string XMLFilePath = "Assets/EditorFramework/Example/7.XMLGUI/Editor/GUIExample.xml";
+        private string mXMLContent;
+        private void OnEnable()
+        {
+            TextAsset xmlFileAsset =  AssetDatabase.LoadAssetAtPath<TextAsset>(XMLFilePath);
+            mXMLContent = xmlFileAsset.text;
+        }
+
+        private void OnGUI()
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(mXMLContent);
+            var rootNode = doc.SelectSingleNode("GUI");
+            foreach (XmlElement rootNodeChildNode in rootNode.ChildNodes)
+            {
+                if (rootNodeChildNode.Name == "Label")
+                {
+                    GUILayout.Label(rootNodeChildNode.InnerText);
+                }
+            }
+        }
+    }
+    
+    
+}
