cb672b647959e4cdfd0fd2d500b55ac48a67ad28
KinoiraLink <summerkino@outlook.com>
2024-06-24 10:30:30 +0800

    SplitView 变换尺寸实现

--- Assets/EditorFramework/Editor/SplitView.cs
+++ Assets/EditorFramework/Editor/SplitView.cs
@@ -7,13 +7,18 @@ namespace EditorFramework
     public class SplitView : GUIBase
     {
         public event Action<Rect> FirstArea, SecondArea;
+
+        public float SplitWidth = 200;
+        public float MinSize = 100;
+
+        public bool Dragging = false;
         public override void OnGUI(Rect position)
         {
             base.OnGUI(position);
             
-            var rects = position.VerticalSplit(200, 4);
+            var rects = position.VerticalSplit(SplitWidth, 4);
 
-            var mid = position.VerticalSplitRect(200, 4);
+            var mid = position.VerticalSplitRect(SplitWidth, 4);
             //右区域
             if(SecondArea != null)
             {
@@ -26,6 +31,40 @@ namespace EditorFramework
             }
         
             EditorGUI.DrawRect(mid.Zoom(AnchorType.MiddleCenter,-2),Color.green);
+
+            var e = Event.current;
+
+            if (mid.Contains(e.mousePosition))
+            {
+                EditorGUIUtility.AddCursorRect(mid,MouseCursor.ResizeHorizontal);
+            }
+
+            switch (e.type)
+            {
+                case EventType.MouseDown:
+                    if (mid.Contains(e.mousePosition))
+                    {
+                        Dragging = true;
+                    }
+                    break;
+                case EventType.MouseDrag:
+                    if (Dragging)
+                    {
+                        SplitWidth += e.delta.x;
+                        
+                        SplitWidth = Mathf.Clamp(SplitWidth, MinSize, position.width -MinSize);
+                        
+                        e.Use();
+                    }
+                    break;
+                
+                case EventType.MouseUp:
+                    if (Dragging)
+                    {
+                        Dragging = false;
+                    }
+                    break;
+            }
         }
 
         protected override void OnDispose()
